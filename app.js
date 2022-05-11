f69619942eb01689eb08eab93b9525615d384ef3
Diyor End <diyorend@gmail.com>
2022-05-11 13:06:46 +0500

    touchcancel

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ container.addEventListener("mouseleave", (e) => {
   img.style.transform = "rotateZ(0deg)";
 });
 
-container.addEventListener("touchstart", (e) => {
+container.addEventListener("touchcancel", (e) => {
   let x = (window.innerWidth / 2 - e.pageX) / 30;
   let y = (window.innerHeight / 2 - e.pageY) / 30;
   card.style.transform = `rotateY(${x}deg) rotateX(${y}deg)`;
