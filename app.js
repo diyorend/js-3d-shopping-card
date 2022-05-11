4f1bc5bc28d7ac90a7753e3201d22f9546dee10c
Diyor End <diyorend@gmail.com>
2022-05-11 12:44:41 +0500

    remove touchmove

--- app.js
+++ app.js
@@ -4,43 +4,35 @@ const title = document.querySelector(".title");
 const img = document.querySelector(".image img");
 const description = document.querySelector("h3");
 
-container.addEventListener("touchmove", (e) => {
-  let x = (window.innerWidth / 2 - e.pageX) / 20;
-  let y = (window.innerHeight / 2 - e.pageY) / 20;
-  card.style.transform = `rotateY(${x}deg) rotateX(${y}deg)`;
-  container.addEventListener("transitionend", () => {
-    img.style.transform = "rotateZ(0deg)";
-    img.style.transition = "all 0.8s ease";
-    card.style.transform = `rotateY(0deg) rotateX(0deg)`;
-    card.style.transition = "all 0.8s ease";
-  });
-});
 container.addEventListener("mousemove", (e) => {
   let x = (window.innerWidth / 2 - e.pageX) / 20;
   let y = (window.innerHeight / 2 - e.pageY) / 20;
   card.style.transform = `rotateY(${x}deg) rotateX(${y}deg)`;
-});
-container.addEventListener("touchstart", (e) => {
-  card.style.transition = "none";
-  img.style.transform = "rotateZ(-30deg)";
-  img.style.transition = "all 0.8s ease";
-  card.style.transition = "all 0.8s ease";
 });
 container.addEventListener("mouseenter", (e) => {
   card.style.transition = "none";
   img.style.transform = "rotateZ(-30deg)";
   img.style.transition = "all 0.8s ease";
 });
-container.addEventListener("touchend", (e) => {
-  container.addEventListener("transitionend", () => {
-    img.style.transform = "rotateZ(0deg)";
-    img.style.transition = "all 0.8s ease";
-    card.style.transform = `rotateY(0deg) rotateX(0deg)`;
-    card.style.transition = "all 0.8s ease";
-  });
-});
 container.addEventListener("mouseleave", (e) => {
   card.style.transition = "all 0.8s ease";
   card.style.transform = `rotateY(0deg) rotateX(0deg)`;
   img.style.transform = "rotateZ(0deg)";
+});
+
+container.addEventListener("touchstart", (e) => {
+  let x = (window.innerWidth / 2 - e.pageX) / 20;
+  let y = (window.innerHeight / 2 - e.pageY) / 20;
+  card.style.transform = `rotateY(${x}deg) rotateX(${y}deg)`;
+  img.style.transform = "rotateZ(-30deg)";
+  img.style.transition = "all 1s ease";
+  card.style.transition = "all 1s ease";
+});
+container.addEventListener("touchend", (e) => {
+  container.addEventListener("transitionend", () => {
+    img.style.transform = "rotateZ(0deg)";
+    img.style.transition = "all 0.8s ease";
+    card.style.transition = "all 0.8s ease";
+    card.style.transform = `rotateY(0deg) rotateX(0deg)`;
+  });
 });
